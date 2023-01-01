f4f05370bb711d926d3d8ecf12fa36a57c6942a9
park sang hyeop <48915018+parksanghyup@users.noreply.github.com>
2023-01-02 04:25:11 +0900

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,6 @@ module.exports = {
 
   plugins: [
     {
-      resolve: `gatsby-plugin-gtag`,
-      options: {
-        trackingId: `G-TLK3HJ99MZ`, // 측정 ID
-        head: false, // head에 tracking script를 넣고 싶다면 true로 변경 
-        anonymize: true,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
@@ -49,7 +41,14 @@ module.exports = {
         },
       },
     },
-    
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `G-TLK3HJ99MZ`, // 측정 ID
+        head: false, // head에 tracking script를 넣고 싶다면 true로 변경
+        anonymize: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
